fe0f8daf14dfbef285dbf19875de2ac944385b2b
sieunhando <tuan.tv1966@sinhvien.hoasen.edu.vn>
2019-11-03 15:41:22 +0700

    update

--- main.go
+++ main.go
@@ -37,15 +37,4 @@ func main() {
 			break
 		}
 	}
-	// fmt.Println(r.Assets)
-	// args.Version = strings.TrimPrefix(args.Version, "v")
-	// r.Name = strings.TrimPrefix(r.Name, "v")
-	// if r.Name != args.Version {
-	// 	fmt.Printf("Not latest. Your Version %s - Latest: %s\n", args.Version, r.Name)
-	// 	if len(r.Assets) > 0 && args.URL {
-	// 		fmt.Printf("URL: %s\n", r.Assets[0].URL)
-	// 	}
-	// 	os.Exit(1)
-	// }
-	// os.Exit(0)
 }
