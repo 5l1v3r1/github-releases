ab17da3e6e8fe56be0694f3eb8d85fd6f9d5ec17
sieunhando <tuan.tv1966@sinhvien.hoasen.edu.vn>
2019-11-04 08:14:04 +0700

    add new functions

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"strings"
 
 	arg "github.com/alexflint/go-arg"
+	"github.com/matishsiao/goInfo"
 )
 
 var args struct {
-	User    string `arg:"required"`
-	Project string `arg:"required"`
-	Distro  string `arg:"" default:"linux" help:"linux/darwin/window"`
-	Lastest bool   `arg:"" help:"Get last version"`
+	User     string `arg:"required"`
+	Project  string `arg:"required"`
+	Kernel   string `arg:"" default:"" help:"linux/darwin/window (Default is your running Kernel)"`
+	Platform string `arg:"" default:"" help:"x86_64/i386"`
+	Lastest  bool   `arg:"" help:"Get last release (Default is Latest Stable Release)"`
+	Download bool   `arg:"" help:"Download file"`
 }
 
 func main() {
@@ -20,6 +26,8 @@ func main() {
 
 	var r Release
 	var err error
+	var downloadLink string
+
 	if args.Lastest {
 		r, err = fetchLatestRelease(args.User, args.Project)
 	} else {
@@ -30,10 +38,65 @@ func main() {
 		os.Exit(1)
 	}
 
+	gi := goInfo.GetInfo()
+	if args.Kernel == "" {
+		args.Kernel = gi.Kernel
+	}
+
 	for i := 0; i < len(r.Assets); i++ {
-		if strings.Contains(strings.ToLower(r.Assets[i].Name), strings.ToLower(args.Distro)) {
-			fmt.Println(r.Assets[i].URL)
+		if strings.Contains(strings.ToLower(r.Assets[i].Name), strings.ToLower(args.Kernel)) {
+			downloadLink = r.Assets[i].URL
 			break
 		}
 	}
+
+	if args.Platform != "" {
+		// Match Platform
+		if strings.Contains(strings.ToLower(downloadLink), strings.ToLower(args.Platform)) {
+			fmt.Println(downloadLink)
+			os.Exit(0)
+		}
+	}
+	fmt.Println(downloadLink)
+
+	if args.Download {
+		fmt.Println("Downloading")
+		err = downloadFile(downloadLink)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Download Finish")
+	}
+}
+
+func downloadFile(downloadUrl string) error {
+	u, err := url.Parse(downloadUrl)
+	if err != nil {
+		return err
+	}
+	path := u.Path
+	segments := strings.Split(path, "/")
+	fileName := segments[len(segments)-1]
+
+	// Get the data
+	resp, err := http.Get(u.String())
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
