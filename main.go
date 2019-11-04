4f81f372d2b516042858ff1a52fafdaa5ef04347
sieunhando <tuan.tv1966@sinhvien.hoasen.edu.vn>
2019-11-04 18:03:56 +0700

    Fix

--- main.go
+++ main.go
@@ -6,19 +6,24 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"runtime"
 	"strings"
 
 	arg "github.com/alexflint/go-arg"
-	"github.com/matishsiao/goInfo"
+)
+
+const (
+	GOARCH string = runtime.GOARCH
+	GOOS   string = runtime.GOOS
 )
 
 var args struct {
-	User     string `arg:"required"`
-	Project  string `arg:"required"`
-	Kernel   string `arg:"" default:"" help:"linux/darwin/window (Default is your running Kernel)"`
-	Platform string `arg:"" default:"" help:"x86_64/i386"`
-	Lastest  bool   `arg:"" help:"Get last release (Default is Latest Stable Release)"`
-	Download bool   `arg:"" help:"Download file"`
+	User         string `arg:"required"`
+	Project      string `arg:"required"`
+	Kernel       string `arg:"" default:"" help:"linux/darwin/window (Default is your running Kernel)"`
+	Architecture string `arg:"" default:"" help:"auto/amd64/386"`
+	Lastest      bool   `arg:"" help:"Get last release (Default is Latest Stable Release)"`
+	Download     bool   `arg:"" help:"Download file"`
 }
 
 func main() {
@@ -38,25 +43,39 @@ func main() {
 		os.Exit(1)
 	}
 
-	gi := goInfo.GetInfo()
 	if args.Kernel == "" {
-		args.Kernel = gi.Kernel
+		args.Kernel = GOOS
 	}
+	fmt.Printf("Your Kernel is: %s\n", args.Kernel)
 
-	for i := 0; i < len(r.Assets); i++ {
-		if strings.Contains(strings.ToLower(r.Assets[i].Name), strings.ToLower(args.Kernel)) {
-			downloadLink = r.Assets[i].URL
-			break
+	matchKernel := []string{}
+	for _, asset := range r.Assets {
+		if strings.Contains(strings.ToLower(asset.Name), strings.ToLower(args.Kernel)) {
+			matchKernel = append(matchKernel, asset.URL)
 		}
 	}
 
-	if args.Platform != "" {
-		// Match Platform
-		if strings.Contains(strings.ToLower(downloadLink), strings.ToLower(args.Platform)) {
-			fmt.Println(downloadLink)
+	if args.Architecture == "auto" {
+		args.Architecture = GOARCH
+	}
+	// Match Architecture
+	if args.Architecture != "" {
+		foundMatch := false
+		for _, link := range matchKernel {
+			if strings.Contains(strings.ToLower(link), strings.ToLower(args.Architecture)) {
+				downloadLink = link
+				foundMatch = true
+				break
+			}
+		}
+
+		if !foundMatch {
+			fmt.Println("Can not found release with your Architecture.")
 			os.Exit(0)
 		}
 	}
+	fmt.Printf("Your Architecture is: %s\n", GOARCH)
+
 	fmt.Println(downloadLink)
 
 	if args.Download {
